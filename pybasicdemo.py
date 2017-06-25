ea93860af19ee20ccd1a3fa806d6c90d4487ff55
d-mahantesh <mahantesh.dhale@unotechsoft.com>
2017-06-25 13:24:16 +0700

    Added slices demo

--- pybasicdemo.py
+++ pybasicdemo.py
@@ -36,5 +36,16 @@ print('*********************************')
 print(word[-1],word[-2],word[-3],word[-4],word[-5],word[-6],word[-7],word[-8],word[0]) #Indexing, check the position
 print('-1''-2''-3''-4''-5''-6''-7''-8'' 0')
 print('*********************************')
-print(word[0:2]) #Print the character from 
-
+print(word[0:2]) #Print the character from 0 (included) till 2 (excluded)
+print(word[3:5]) #Print the character from 3 (included) till 5 (excluded)
+print(word[:2]+word[1:]) #This is print(word[:i]+word[i:]) is always equal to print(word)
+print(word[:2]) #This printing character from beginning to 2 excluded
+print(word[4:]) #This printing character from 4 included to end
+print(word[-2:]) #This printing character from second-last included to end
+print(word[:-2]) #This printing character from beginning to second-last excluded to end
+#print(word[42]) this will throw index out of range
+print(word[2:42]) #Out of index range is handled gracefully
+#word[0] = 'J' python string cannot be changed they are immutable
+print('J' + word[1:]) #can build a new string
+print('J' + word[-7])
+print(len(word)) #length of the string
