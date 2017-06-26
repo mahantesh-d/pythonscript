a1bed6cb11ffb1517c33cc94d0ef13e8e457ede2
d-mahantesh <mahantesh.dhale@unotechsoft.com>
2017-06-26 11:53:11 +0700

    Added more features like Content-Lenght, Response-Time, Request-URL, Status-Code

--- getreq.py
+++ getreq.py
@@ -1,6 +1,11 @@
 import requests
-r = requests.get("https://httpbin.org/get")
-#print (r.status_code)
-#print (r.headers)
-#print (r.content)
-print (r.json())
+r = requests.get("http://10.252.169.12:7788/v1/product/productSpecification/productMaster.json?filter=(|(matCode_key15=qwqw17)(company_key0=asas10))")
+r.close()
+print('Request_URL: ', r.url)
+print ('Status Code: ',r.status_code)
+#print ('Headers: ',r.headers)
+print ('Content-Length: ',r.headers.get('Content-Length'))
+print('Response_Time: ',r.elapsed)
+#print (r.json())
+#a = {'abc':1, 'abcd':'2'}
+#print(a.get('abcd'))
