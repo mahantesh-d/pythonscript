bd5ff930c3dc9fa7483b6aea1f3c5f35d55f0af2
d-mahantesh <mahantesh.dhale@unotechsoft.com>
2017-06-26 13:56:30 +0700

    Added Error Handling, exit(1)

--- getreq.py
+++ getreq.py
@@ -1,5 +1,9 @@
 import requests
-r = requests.get("http://10.252.169.12:7788/v1/product/productSpecification/productMaster.json?filter=(|(matCode_key15=qwqw17)(company_key0=asas10))")
+try:
+    r = requests.get("http://10.252.169.12:7788/v1/product/productSpecification/productMaster.json?filter=(|(matCode_key15=qwqw17)(company_key0=asas10))")
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print (e)
+    exit(1)
 r.close()
 print('Request_URL: ', r.url)
 print ('Status Code: ',r.status_code)
@@ -9,3 +13,4 @@ print('Response_Time: ',r.elapsed)
 #print (r.json())
 #a = {'abc':1, 'abcd':'2'}
 #print(a.get('abcd'))
+print("Host not found")
