4a827f2e45cb42239476193be90ac7207223a5ae
d-mahantesh <mahantesh.dhale@unotechsoft.com>
2017-07-01 00:57:24 +0700

    One way of reading the excel file

---
+++ readingexcelxlrd.py
@@ -0,0 +1,14 @@
+import xlrd
+book = xlrd.open_workbook('sample.xlsx')
+
+max_nb_row = 0
+for sheet in book.sheets():
+    max_nb_row = max(max_nb_row, sheet.nrows)
+
+for row in range(max_nb_row) :
+    print(row)
+    for sheet in book.sheets() :
+       if row < sheet.nrows :
+           print(sheet.nrows)
+           a = sheet.row(row)
+           print(a)
